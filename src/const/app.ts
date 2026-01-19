9a0b3548ec0feb13d08f3a089a58281a5442766a
Mikku44 <coverblack12345@gmail.com>
2026-01-19 17:33:34 +0700

    feat : initialize project

---
+++ src/const/app.ts
@@ -0,0 +1,58 @@
+export const MENU_ITEMS = [
+  { name: "แนะนำแนวข้อสอบ", link: "#exam" },
+  { name: "Mock Test", link: "#mock-test" },
+  { name: "สนใจลองทำ", link: "#try-it" },
+];
+
+
+export const BOOK_ITEMS = [
+  {
+    title: "ตะลุยโจทย์ CLOZE 500 ข้อ",
+    description: "สรุปเข้ม GRAMMA & VOCAB พร้อมแนวข้อสอบ CLOZE 500 ข้อ ครบทุกประเด็น",
+    price: "246",
+    badge: "MUST READ",
+    image: "/books/1.jpg",
+    status: "เล่มพิมพ์นิยม",
+    link : "https://s.shopee.co.th/1BEUd5z4OT"
+  },
+  {
+    title: "กรมศุลกากร เตรียมสอบทุกตำแหน่ง 2569",
+    description: "อัปเดตพิกัดล่าสุด 2569 แยกหมวดหมู่ชัดเจน อ่านจบทำข้อสอบพิกัดได้แน่นอน",
+    price: "500",
+    badge: "",
+    image: "/books/2.jpg",
+    status: "ขายดีที่สุด",
+    link : "https://s.shopee.co.th/2g3I94HYBx"
+  },
+  {
+    title: "ความรู้ทั่วไป & TOEIC นักวิชาการศุลกากร 2569",
+    description: "รวมทุกเรื่องที่กรมศุลกากรให้ความสำคัญ TOEIC 100 เทคนิค",
+    price: "545",
+    badge: "NEW",
+    image: "/books/3.jpg",
+    status: "เนื้อหาอัปเดต",
+    link : "https://s.shopee.co.th/VyluWXnQu"
+  }
+];
+
+
+export const PREVIEW_IMAGES = [
+  {
+    title: "รูปแบบ Google Form ใช้งานง่าย",
+    description: "หน้าตา Interface สะอาดตา แบ่งหมวดหมู่ชัดเจน ทำได้ลื่นไหลทั้งใน iOS และ Android",
+    image: "/exams/1.jpg", // เปลี่ยนเป็นพาธรูปของคุณ
+    tag: "Interface"
+  },
+  {
+    title: "เฉลยละเอียด พร้อมแหล่งอ้างอิง",
+    description: "ทุกข้อที่ตอบผิด จะมีคำอธิบายกฎหมายที่เกี่ยวข้อง เพื่อให้คุณเข้าใจที่มา ไม่ใช่แค่การจำคำตอบ",
+    image: "/exams/2.jpg",
+    tag: "Explanation"
+  },
+  {
+    title: "วิเคราะห์คะแนนทันที",
+    description: "รู้จุดแข็งและจุดที่ต้องพัฒนาทันทีหลังส่งคำตอบ ช่วยประหยัดเวลาในการทบทวน",
+    image: "/exams/3.jpg",
+    tag: "Score Report"
+  }
+];
